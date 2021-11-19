0b6d9ad577363a786beb012ae8456d90159fa575
Sanier <Sanier20@mail.ru>
2021-11-19 15:41:11 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace ParsTxt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("C:\\Users\\sanie\\Desktop\\cdev_Text.txt");
+
+            char[] delimiters = new char[] {' ', '\r', '\n' };
+
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine(words.Length);
+        }
+    }
+}
